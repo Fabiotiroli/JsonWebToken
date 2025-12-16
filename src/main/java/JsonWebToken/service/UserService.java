c260cada3987096dda076765fefa2173463c526a
Fabio Tiroli <fabiolobotiroli@gmail.com>
2025-12-16 14:24:46 -0300

    First

---
+++ src/main/java/JsonWebToken/service/UserService.java
@@ -0,0 +1,26 @@
+package JsonWebToken.service;
+
+import JsonWebToken.model.User;
+import JsonWebToken.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    private PasswordEncoder encoder;
+
+    public void createUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        repository.save(user);
+
+
+
+    }
+}
